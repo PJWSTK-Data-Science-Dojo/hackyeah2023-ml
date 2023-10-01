237aaed6f4bc45f52a1984e413221b288eec6b21
Pexo <contact@pexogamedev.pl>
2023-10-01 04:47:57 +0200

    Fixed build commit

--- BuildDockerAndRun.bat
+++ BuildDockerAndRun.bat
@@ -2,4 +2,4 @@ docker stop hackyeah-ml
 docker rm hackyeah-ml
 docker rmi hackyeah-ml
 docker build -t hackyeah-ml .
-docker run -d -p 6969:105 --name hackyeah-ml hackyeah-ml
+docker run -d -p 7000:105 --name hackyeah-ml hackyeah-ml
